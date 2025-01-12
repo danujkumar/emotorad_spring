7ab085cacb3c16e951cb98df359a8e2e19d2d559
Anuj Singh <anuj.as828@gmail.com>
2025-01-12 15:17:14 +0530

    initialized spring

---
+++ src/main/java/com/emotorad/Emotorad/EmotoradApplication.java
@@ -0,0 +1,13 @@
+package com.emotorad.Emotorad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotoradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotoradApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emotorad/Emotorad/EmotoradApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emotorad.Emotorad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotoradApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
