82669ecdaecb3ff78f2e3d7b389cd0bcf6d32c22
Anuj Singh <anuj.as828@gmail.com>
2025-01-18 12:17:46 +0530

    env created

--- src/main/java/com/emotorad/Emotorad/EmotoradApplication.java
+++ src/main/java/com/emotorad/Emotorad/EmotoradApplication.java
@@ -1,6 +1,7 @@
 package com.emotorad.Emotorad;
 
 import org.springframework.boot.SpringApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -8,6 +9,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class EmotoradApplication {
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("MONGO_ID", dotenv.get("MONGO_ID"));
+		System.setProperty("MONGO_PASSWORD", dotenv.get("MONGO_PASSWORD"));
+
 		SpringApplication.run(EmotoradApplication.class, args);
 	}
 }
